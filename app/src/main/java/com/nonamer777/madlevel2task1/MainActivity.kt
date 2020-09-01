94de0fdd46d1ca004c08e2f354ee0d9aae58a078
Oscar Wellner <o.wellner@telfort.nl>
2020-09-01 18:18:41 +0200

    Step 3: added logic to load and show the data in the staggered grid

--- app/src/main/java/com/nonamer777/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/nonamer777/madlevel2task1/MainActivity.kt
@@ -2,10 +2,38 @@ package com.nonamer777.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.nonamer777.madlevel2task1.databinding.ActivityMainBinding
+import com.nonamer777.madlevel2task1.model.Place
+import com.nonamer777.madlevel2task1.model.PlaceAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(
+            2,
+            LinearLayoutManager.VERTICAL
+        )
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanged()
     }
 }
